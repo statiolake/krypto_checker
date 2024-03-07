91c35903228db19ca8badea0887a02e29065bd7d
statiolake <statiolake@gmail.com>
2024-03-07 20:31:59 +0900

    refactor: extract core algorithms into library

---
+++ src/lib.rs
@@ -0,0 +1,209 @@
+use std::{
+    collections::{HashMap, HashSet},
+    iter,
+    sync::Mutex,
+};
+
+use itertools::Itertools;
+use num_rational::Rational64;
+use once_cell::sync::Lazy;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Formula<T> {
+    Leaf(T),
+    Add(Box<Formula<T>>, Box<Formula<T>>),
+    Sub(Box<Formula<T>>, Box<Formula<T>>),
+    Mul(Box<Formula<T>>, Box<Formula<T>>),
+    Div(Box<Formula<T>>, Box<Formula<T>>),
+}
+
+pub type IndexFormula = Formula<usize>;
+pub type AssignedFormula = Formula<i64>;
+
+#[derive(thiserror::Error, Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[error("division by zero")]
+pub struct ZeroDivisionError;
+
+impl IndexFormula {
+    fn apply(&self, values: &[i64]) -> Result<Rational64, ZeroDivisionError> {
+        match self {
+            Formula::Leaf(index) => Ok(Rational64::new(values[*index], 1)),
+            Formula::Add(left, right) => Ok(left.apply(values)? + right.apply(values)?),
+            Formula::Sub(left, right) => Ok(left.apply(values)? - right.apply(values)?),
+            Formula::Mul(left, right) => Ok(left.apply(values)? * right.apply(values)?),
+            Formula::Div(left, right) => {
+                let left = left.apply(values)?;
+                let right = right.apply(values)?;
+                if right != Rational64::new(0, 1) {
+                    Ok(left / right)
+                } else {
+                    Err(ZeroDivisionError)
+                }
+            }
+        }
+    }
+
+    fn assign(&self, values: &[i64]) -> AssignedFormula {
+        match self {
+            Formula::Leaf(index) => Formula::Leaf(values[*index]),
+            Formula::Add(left, right) => Formula::Add(
+                Box::new(left.assign(values)),
+                Box::new(right.assign(values)),
+            ),
+            Formula::Sub(left, right) => Formula::Sub(
+                Box::new(left.assign(values)),
+                Box::new(right.assign(values)),
+            ),
+            Formula::Mul(left, right) => Formula::Mul(
+                Box::new(left.assign(values)),
+                Box::new(right.assign(values)),
+            ),
+            Formula::Div(left, right) => Formula::Div(
+                Box::new(left.assign(values)),
+                Box::new(right.assign(values)),
+            ),
+        }
+    }
+}
+
+impl AssignedFormula {
+    pub fn compute(&self) -> Result<Rational64, ZeroDivisionError> {
+        match self {
+            Formula::Leaf(value) => Ok(Rational64::new(*value, 1)),
+            Formula::Add(left, right) => Ok(left.compute()? + right.compute()?),
+            Formula::Sub(left, right) => Ok(left.compute()? - right.compute()?),
+            Formula::Mul(left, right) => Ok(left.compute()? * right.compute()?),
+            Formula::Div(left, right) => {
+                let left = left.compute()?;
+                let right = right.compute()?;
+                if right != Rational64::new(0, 1) {
+                    Ok(left / right)
+                } else {
+                    Err(ZeroDivisionError)
+                }
+            }
+        }
+    }
+}
+
+fn enumerate_formulas(indices: &[usize]) -> Vec<IndexFormula> {
+    if indices.len() == 1 {
+        return vec![Formula::Leaf(indices[0])];
+    }
+
+    let mut res = vec![];
+    for break_at in 0..indices.len() {
+        let left_indices = &indices[..break_at];
+        let right_indices = &indices[break_at..];
+
+        if left_indices.is_empty() || right_indices.is_empty() {
+            continue;
+        }
+
+        for left_formula in enumerate_formulas(left_indices) {
+            for right_formula in enumerate_formulas(right_indices) {
+                res.push(Formula::Add(
+                    Box::new(left_formula.clone()),
+                    Box::new(right_formula.clone()),
+                ));
+                res.push(Formula::Sub(
+                    Box::new(left_formula.clone()),
+                    Box::new(right_formula.clone()),
+                ));
+                res.push(Formula::Mul(
+                    Box::new(left_formula.clone()),
+                    Box::new(right_formula.clone()),
+                ));
+                res.push(Formula::Div(
+                    Box::new(left_formula.clone()),
+                    Box::new(right_formula.clone()),
+                ));
+            }
+        }
+    }
+
+    res
+}
+
+pub fn get_formulas_cached(n: usize) -> Vec<IndexFormula> {
+    static FORMULAS_CACHE: Lazy<Mutex<HashMap<usize, Vec<IndexFormula>>>> =
+        Lazy::new(|| Mutex::new(HashMap::new()));
+
+    assert!(n <= 5, "too large n");
+
+    {
+        let cache = FORMULAS_CACHE.lock().unwrap();
+        if let Some(formulas) = cache.get(&n) {
+            // Clone cached formulas if already computed for given n
+            return formulas.clone();
+        }
+    }
+
+    // Compute and insert cache for this n
+    let formulas = enumerate_formulas(&(0..n).collect_vec());
+
+    {
+        let mut cache = FORMULAS_CACHE.lock().unwrap();
+        cache.insert(n, formulas.clone());
+    }
+
+    formulas
+}
+
+pub fn find_answers(cards: &[i64], target: i64) -> impl Iterator<Item = AssignedFormula> {
+    // Clippy complains about unnecessary conversion to owned value, but it is indeed needed to
+    // remove lifetime from the returning iterator.
+    #[allow(clippy::unnecessary_to_owned)]
+    let mut permutations = cards
+        .to_vec()
+        .into_iter()
+        .permutations(cards.len())
+        .unique();
+    let formulas = get_formulas_cached(cards.len());
+
+    let mut current_permutation = permutations.next();
+    let mut current_formulas = formulas.clone().into_iter();
+
+    iter::from_fn(move || loop {
+        let Some(formula) = current_formulas.next() else {
+            current_permutation = permutations.next();
+            current_formulas = formulas.clone().into_iter();
+            continue;
+        };
+
+        let Some(permutation) = current_permutation.clone() else {
+            return None;
+        };
+
+        let Ok(value) = formula.apply(&permutation) else {
+            continue;
+        };
+
+        if value == Rational64::from(target) {
+            return Some(formula.assign(&permutation));
+        }
+    })
+}
+
+pub fn compute_impossibles(cards: &[i64], within: HashSet<i64>) -> HashSet<i64> {
+    let mut impossibles = within;
+    let formulas = get_formulas_cached(cards.len());
+
+    'order_loop: for ordered in cards.iter().copied().permutations(cards.len()).unique() {
+        for formula in &formulas {
+            let Ok(value) = formula.apply(&ordered) else {
+                continue;
+            };
+
+            if value.is_integer() {
+                impossibles.remove(&value.to_integer());
+            }
+
+            if impossibles.is_empty() {
+                break 'order_loop;
+            }
+        }
+    }
+
+    impossibles
+}
--- src/main.rs
+++ src/main.rs
@@ -1,175 +1,12 @@
-use std::{
-    collections::HashSet,
-    ops::{Add, Div, Mul, Sub},
-};
+use std::collections::HashSet;
 
 use itertools::{repeat_n, Itertools};
 
-const MAX_CARD_NUMBER: i32 = 10;
+use krypto_checker::compute_impossibles;
+
+const MAX_CARD_NUMBER: i64 = 10;
 const CARD_DUPLICATES: usize = 3;
 const NUM_HAND_CARDS: usize = 5;
-
-#[derive(Debug, Clone, Copy, Eq, PartialOrd, Ord)]
-pub struct Rational {
-    num: i32,
-    den: i32,
-}
-
-impl Rational {
-    pub fn new(num: i32, den: i32) -> Self {
-        assert_ne!(den, 0);
-        Rational { num, den }
-    }
-}
-
-impl Add for Rational {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Rational::new(
-            self.num * other.den + other.num * self.den,
-            self.den * other.den,
-        )
-    }
-}
-
-impl Sub for Rational {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        Rational::new(
-            self.num * other.den - other.num * self.den,
-            self.den * other.den,
-        )
-    }
-}
-
-impl Mul for Rational {
-    type Output = Self;
-
-    fn mul(self, other: Self) -> Self {
-        Rational::new(self.num * other.num, self.den * other.den)
-    }
-}
-
-impl Div for Rational {
-    type Output = Self;
-
-    fn div(self, other: Self) -> Self {
-        Rational::new(self.num * other.den, self.den * other.num)
-    }
-}
-
-impl PartialEq for Rational {
-    fn eq(&self, other: &Self) -> bool {
-        self.num * other.den == other.num * self.den
-    }
-}
-
-impl TryFrom<Rational> for i32 {
-    type Error = Rational;
-
-    fn try_from(value: Rational) -> Result<Self, Self::Error> {
-        if value.num % value.den == 0 {
-            Ok(value.num / value.den)
-        } else {
-            Err(value)
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
-enum Formula {
-    Leaf(usize),
-    Add(Box<Formula>, Box<Formula>),
-    Sub(Box<Formula>, Box<Formula>),
-    Mul(Box<Formula>, Box<Formula>),
-    Div(Box<Formula>, Box<Formula>),
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct ZeroDivisionError;
-
-impl Formula {
-    fn apply(&self, values: &[i32]) -> Result<Rational, ZeroDivisionError> {
-        match self {
-            Formula::Leaf(index) => Ok(Rational::new(values[*index], 1)),
-            Formula::Add(left, right) => Ok(left.apply(values)? + right.apply(values)?),
-            Formula::Sub(left, right) => Ok(left.apply(values)? - right.apply(values)?),
-            Formula::Mul(left, right) => Ok(left.apply(values)? * right.apply(values)?),
-            Formula::Div(left, right) => {
-                let left = left.apply(values)?;
-                let right = right.apply(values)?;
-                if right != Rational::new(0, 1) {
-                    Ok(left / right)
-                } else {
-                    Err(ZeroDivisionError)
-                }
-            }
-        }
-    }
-}
-
-fn enumerate_formulas(indices: &[usize]) -> Vec<Formula> {
-    if indices.len() == 1 {
-        return vec![Formula::Leaf(indices[0])];
-    }
-
-    let mut res = vec![];
-    for break_at in 0..indices.len() {
-        let left_indices = &indices[..break_at];
-        let right_indices = &indices[break_at..];
-
-        if left_indices.is_empty() || right_indices.is_empty() {
-            continue;
-        }
-
-        for left_formula in enumerate_formulas(left_indices) {
-            for right_formula in enumerate_formulas(right_indices) {
-                res.push(Formula::Add(
-                    Box::new(left_formula.clone()),
-                    Box::new(right_formula.clone()),
-                ));
-                res.push(Formula::Sub(
-                    Box::new(left_formula.clone()),
-                    Box::new(right_formula.clone()),
-                ));
-                res.push(Formula::Mul(
-                    Box::new(left_formula.clone()),
-                    Box::new(right_formula.clone()),
-                ));
-                res.push(Formula::Div(
-                    Box::new(left_formula.clone()),
-                    Box::new(right_formula.clone()),
-                ));
-            }
-        }
-    }
-
-    res
-}
-
-fn compute_impossibles(formulas: &[Formula], cards: &[i32]) -> HashSet<i32> {
-    let mut impossibles: HashSet<i32> = (1..=MAX_CARD_NUMBER).collect();
-
-    'order_loop: for order in cards.iter().copied().permutations(5).unique() {
-        for formula in formulas {
-            let Ok(value) = formula.apply(&order) else {
-                continue;
-            };
-
-            if let Ok(integer) = i32::try_from(value) {
-                impossibles.remove(&integer);
-            }
-
-            if impossibles.is_empty() {
-                break 'order_loop;
-            }
-        }
-    }
-
-    impossibles
-}
 
 fn main() {
     let entire = repeat_n(1..=MAX_CARD_NUMBER, CARD_DUPLICATES)
@@ -177,10 +14,10 @@ fn main() {
         .sorted()
         .collect_vec();
     let combs = entire.iter().copied().combinations(NUM_HAND_CARDS).unique();
-    let formulas = enumerate_formulas(&(0..NUM_HAND_CARDS).collect_vec());
+    let within: HashSet<_> = entire.iter().copied().collect();
 
     for hand in combs {
-        let impossibles = compute_impossibles(&formulas, &hand);
+        let impossibles = compute_impossibles(&hand, within.clone());
         if !impossibles.is_empty() {
             println!("{hand:?} != {impossibles:?}");
         }
